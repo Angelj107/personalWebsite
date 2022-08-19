e5e91ded848a2c93361303a0ee549e96751e5536
Angelj107 <jordangelx@gmail.com>
2022-08-19 15:52:40 +0200

    AudioPlayerRevised

---
+++ audioPlayer.js
@@ -0,0 +1,166 @@
+import lottieWeb from 'https://cdn.skypack.dev/lottie-web'
+document.querySelector('#originalsLink').addEventListener('click', displayAudio, {once : true})
+export function displayAudio() {
+  /** Implementation of the presentation of the audio player */
+  const playIconContainer = document.getElementsByClassName('play-icon')
+  const seekSlider = document.getElementsByClassName('seek-slider')
+  const volumeSlider = document.getElementsByClassName('volume-slider')
+  const muteIconContainer = document.getElementsByClassName('mute-icon')
+  let playState = Array(playIconContainer.length).fill('play')
+  let muteState = Array(playIconContainer.length).fill('unmute')
+  const addPlayIcon = () => {
+    ;[...playIconContainer].forEach((el) => {
+      let elNumber = el.closest('div').querySelector('audio').dataset.songnumber
+      var playAnimation = lottieWeb.loadAnimation({
+        container: el,
+        path: 'https://maxst.icons8.com/vue-static/landings/animated-icons/icons/pause/pause.json',
+        renderer: 'svg',
+        loop: false,
+        autoplay: false,
+        name: 'Play Animation',
+      })
+      playAnimation.goToAndStop(14, true)
+      let elPlayState = playState[elNumber]
+      el.addEventListener('click', () => {
+        if (elPlayState === 'play') {
+          playAnimation.playSegments([14, 27], true)
+          elPlayState = 'pause'
+          el.closest('div').querySelector('audio').play()
+          updateTime(elPlayState, elNumber)
+        } else {
+          playAnimation.playSegments([0, 14], true)
+          elPlayState = 'play'
+          el.closest('div').querySelector('audio').pause()
+        }
+      })
+    })
+  }
+  addPlayIcon()
+  const addMuteIcon = () => {
+    ;[...muteIconContainer].forEach((el) => {
+      var muteAnimation = lottieWeb.loadAnimation({
+        container: el,
+        path: 'https://maxst.icons8.com/vue-static/landings/animated-icons/icons/mute/mute.json',
+        renderer: 'svg',
+        loop: false,
+        autoplay: false,
+        name: 'Mute Animation',
+      })
+      let elNumber = el.closest('div').querySelector('audio').dataset.songnumber
+      let elMuteState = muteState[elNumber]
+      el.addEventListener('click', () => {
+        if (elMuteState === 'unmute') {
+          muteAnimation.playSegments([0, 15], true)
+          el.closest('div').querySelector('audio').muted = true
+          elMuteState = 'mute'
+        } else {
+          muteAnimation.playSegments([15, 25], true)
+          el.closest('div').querySelector('audio').muted = false
+          elMuteState = 'unmute'
+        }
+      })
+    })
+  }
+  addMuteIcon()
+
+  var updateTime = (element, id) => {
+    var previousTime
+    var intervalFunction = setInterval(function () {
+      var sliderElement = seekSlider[id]
+      var audioElement = sliderElement.closest('div').querySelector('audio')
+      var currentTime = audioElement.currentTime
+      var timeElement = sliderElement
+        .closest('div')
+        .querySelector('.current-time')
+      sliderElement.value = (currentTime / audioElement.duration) * 100
+      if (currentTime != previousTime) {
+        timeElement.innerHTML = calculateTime(currentTime)
+        previousTime = currentTime
+      } else {
+        clearInterval(intervalFunction)
+      }
+    }, 500)
+  }
+  const showRangeProgress = (rangeInput, element) => {
+    var seekSliderEl = element.closest('div').querySelector('.seek-slider')
+    if (rangeInput === seekSliderEl)
+      element.style.setProperty(
+        '--seek-before-width',
+        (rangeInput.value / rangeInput.max) * 100 + '%'
+      )
+    else
+      element.style.setProperty(
+        '--volume-before-width',
+        (rangeInput.value / rangeInput.max) * 100 + '%'
+      )
+  }
+
+  const seekSliderEvent = () => {
+    ;[...seekSlider].forEach((el) => {
+      el.addEventListener('input', (e) => {
+        var audioSrc = el.closest('div').querySelector('audio');
+        el.closest('div').querySelector('.current-time').innerHTML =
+          calculateTime((audioSrc.duration * e.target.value) / 100)
+        audioSrc.currentTime =audioSrc.duration*e.target.value/100
+        showRangeProgress(e.target, el)
+      })
+    })
+  }
+  seekSliderEvent()
+  const volumeSliderEvent = () => {
+    ;[...volumeSlider].forEach((el) => {
+      el.addEventListener('input', (e) => {
+        el.closest('div').querySelector('.volume-output').innerHTML =
+          Math.round((e.target.value / e.target.max) * 100) + '%'
+        el.closest('div').querySelector('audio').volume = e.target.value / 100
+        showRangeProgress(e.target, el)
+      })
+    })
+  }
+  volumeSliderEvent()
+  /** Implementation of the functionality of the audio player */
+
+  const audio = document.getElementsByClassName('audioPlayerItem')
+
+  const calculateTime = (secs) => {
+    const minutes = Math.floor(secs / 60)
+    const seconds = Math.floor(secs % 60)
+    const returnedSeconds = seconds < 10 ? `0${seconds}` : `${seconds}`
+    return `${minutes}:${returnedSeconds}`
+  }
+
+  const displayDuration = (element) => {
+    var durationCont = element.closest('div').querySelector('.duration')
+    durationCont.textContent = calculateTime(element.duration)
+  }
+
+  const setSliderMax = (element) => {
+    console.log(element)
+    seekSlider.max = Math.floor(element.duration)
+  }
+
+  const displayBufferedAmount = (element) => {
+    const bufferedAmount = Math.floor(
+      element.buffered.end(element.buffered.length - 1)
+    )
+    var parent = element.closest('.audio-player-container')
+    parent.style.setProperty(
+      '--buffered-width',
+      `${(bufferedAmount / seekSlider.max) * 100}%`
+    )
+  }
+  ;[...audio].forEach((el) => {
+    if (el.readyState > 0) {
+      displayDuration(el)
+      setSliderMax(el)
+      displayBufferedAmount(el)
+    } else {
+      el.addEventListener('loadedmetadata', () => {
+        displayDuration(el)
+        setSliderMax(el)
+        displayBufferedAmount(el)
+      })
+    }
+    el.addEventListener('progress', displayBufferedAmount(el))
+  })
+}
---
+++ main.js
@@ -0,0 +1,154 @@
+
+const showMore = (id) => {
+  var moreText = document.getElementsByClassName('more')[id]
+  var btnText = document.getElementsByClassName('moreButton')[id]
+  if (moreText.style.display != 'inline') {
+    moreText.style.display = 'inline'
+    btnText.innerHTML = ' - '
+    btnText.style.color = 'red'
+  } else {
+    moreText.style.display = 'none'
+    btnText.innerHTML = ' ... '
+    btnText.style.color = 'green'
+  }
+}
+var navMargin = document.getElementById('navEl').offsetHeight
+var sections = document.getElementsByClassName('section')
+var musicSections = document.getElementsByClassName('musicSection')
+var home = document.getElementById('pages')
+home.style.marginTop = navMargin + 'px'
+home.style.height = home.offsetHeight - navMargin + 'px'
+document.body.style.height = document.body.offsetHeight - navMargin + 'px'
+const pageShow = (id) => {
+  ;[...sections].forEach((el) => {
+    el.style.display = 'none'
+  })
+  document.getElementById(id).style.display = 'block'
+}
+const active = (link) => {
+  var links = document.getElementsByClassName('navLink')
+  ;[...links].forEach((e) => {
+    e.className = 'navLink'
+  })
+  console.log(links)
+  link.className = 'navLink active'
+}
+const musicPageShow = (id) => {
+  [...musicSections].forEach((el) => {
+    el.style.display = 'none'
+  })
+  document.getElementById(id).style.display = 'block'
+}
+const showSlideshow = (id) => {
+  document.getElementById(id).style.display = 'block'
+}
+const closeSlideshow = (id) => {
+  document.getElementById(id).style.display = 'none'
+}
+
+/* Carousel script from here. Credits to @w3schools */
+let slideIndex = [1, 1, 1]
+let slideId = ['mySlides1', 'mySlides2', 'mySlides3']
+
+async function foo() {
+  var obj
+  const res = await fetch('./data.json')
+  obj = await res.json()
+  let photoshop = document.getElementById('photoshopSlideshow')
+  let illustrator = document.getElementById('illustratorSlideshow')
+  let blender = document.getElementById('blenderSlideshow')
+  let covers = document.getElementById('covers')
+  let originals = document.getElementById('originals')
+  let i = 0;
+  obj.data.forEach((el) => {
+    var div = document.createElement('div')
+    if (
+      el.extension === 'jpg' ||
+      el.extension === 'png' ||
+      el.extension === 'svg'
+    ) {
+      var image = document.createElement('img')
+      image.src = 'src/' + el.source + '/' + el.fileName + '.' + el.extension
+      if (image.style.height > image.style.width) {
+        image.style.height = '100%'
+      } else {
+        image.style.width = '100%'
+      }
+      div.appendChild(image)
+    }
+    if (el.extension === 'mp4') {
+      div.innerHTML =
+        '<h2>' +
+        el.title +
+        "</h2><video width='100%' controls><source src='src/" +
+        el.source +
+        '/' +
+        el.fileName +
+        '.' +
+        el.extension +
+        "'</video>"
+    }
+    if (el.extension === 'mp3') {
+      div.className = 'audio-player-container'
+      div.innerHTML =
+        '<p class = pAudioPlayer>' +
+        el.title +
+        "</p><audio style='display:none' data-songnumber='" +
+        i +
+        "' controls preload='metadata' class='audioPlayerItem' src='src/" +
+        el.source +
+        '/' +
+        el.fileName +
+        '.' +
+        el.extension +
+        "'></audio>" +
+        "<button class='play-icon'></button>" +
+        "<span class = 'current-time time'> 0:00</span>" +
+        "<input type='range' class='seek-slider' max='100' value='0'>" +
+        "<span class='duration time'>0:00</span>" +
+        "<output class='volume-output'>100%</output>" +
+        "<input type='range' class='volume-slider' max='100' value='100'>" +
+        "<button class='mute-icon'></button>"
+      i++;
+    }
+    if (el.source == 'photoshopSlides') {
+      div.className = 'mySlides1'
+      photoshop.appendChild(div)
+    }
+    if (el.source == 'illustratorSlides') {
+      div.className = 'mySlides2'
+      illustrator.appendChild(div)
+    }
+    if (el.source == 'blenderSlides') {
+      div.className = 'mySlides3'
+      blender.appendChild(div)
+    }
+    if (el.source == 'musicVideos') {
+      covers.appendChild(div)
+    }
+    if (el.source == 'originalAudio') {
+      originals.appendChild(div)
+    }
+  })
+  showSlides(1, 0)
+  showSlides(1, 1)
+  showSlides(1, 2)
+}
+function showSlides(n, no) {
+  let i
+  let x = document.getElementsByClassName(slideId[no])
+  if (n > x.length) {
+    slideIndex[no] = 1
+  }
+  if (n < 1) {
+    slideIndex[no] = x.length
+  }
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = 'none'
+  }
+  x[slideIndex[no] - 1].style.display = 'block'
+}
+function plusSlides(n, no) {
+  showSlides((slideIndex[no] += n), no)
+}
+foo()
